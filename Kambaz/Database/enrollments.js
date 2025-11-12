457411ce9b4b0f0453296dcf5fb6935efeb1f4a3
Sanan Moinuddin <mohammedsanan18@gmail.com>
2025-11-12 01:43:06 -0500

    Enroll all faculty and admin in all courses by default
    - Faculty users (690, 901, 902) now enrolled in all 8 courses
    - Admin user (1000) now enrolled in all 8 courses
    - This ensures they always have access regardless of server restarts

--- Kambaz/Database/enrollments.js
+++ Kambaz/Database/enrollments.js
@@ -1,18 +1,56 @@
 export default [
+  // Student enrollments
   { _id: "1", user: "123", course: "RS101" },
   { _id: "2", user: "234", course: "RS101" },
   { _id: "3", user: "345", course: "RS101" },
   { _id: "4", user: "456", course: "RS101" },
   { _id: "5", user: "567", course: "RS101" },
   { _id: "6", user: "678", course: "RS101" },
-  { _id: "7", user: "901", course: "RS101" },
   { _id: "8", user: "234", course: "RS102" },
   { _id: "9", user: "789", course: "RS102" },
   { _id: "10", user: "890", course: "RS102" },
   { _id: "11", user: "123", course: "RS102" },
-  { _id: "12", user: "902", course: "RS102" },
   { _id: "13", user: "345", course: "RS103" },
   { _id: "14", user: "456", course: "RS103" },
   { _id: "15", user: "567", course: "RS103" },
-  { _id: "16", user: "901", course: "RS103" },
+
+  // Faculty 690 (capt_america) - all courses
+  { _id: "100", user: "690", course: "RS101" },
+  { _id: "101", user: "690", course: "RS102" },
+  { _id: "102", user: "690", course: "RS103" },
+  { _id: "103", user: "690", course: "RS104" },
+  { _id: "104", user: "690", course: "RS105" },
+  { _id: "105", user: "690", course: "RS106" },
+  { _id: "106", user: "690", course: "RS107" },
+  { _id: "107", user: "690", course: "RS108" },
+
+  // Faculty 901 (hulk) - all courses
+  { _id: "110", user: "901", course: "RS101" },
+  { _id: "111", user: "901", course: "RS102" },
+  { _id: "112", user: "901", course: "RS103" },
+  { _id: "113", user: "901", course: "RS104" },
+  { _id: "114", user: "901", course: "RS105" },
+  { _id: "115", user: "901", course: "RS106" },
+  { _id: "116", user: "901", course: "RS107" },
+  { _id: "117", user: "901", course: "RS108" },
+
+  // Faculty 902 (thor_odinson) - all courses
+  { _id: "120", user: "902", course: "RS101" },
+  { _id: "121", user: "902", course: "RS102" },
+  { _id: "122", user: "902", course: "RS103" },
+  { _id: "123", user: "902", course: "RS104" },
+  { _id: "124", user: "902", course: "RS105" },
+  { _id: "125", user: "902", course: "RS106" },
+  { _id: "126", user: "902", course: "RS107" },
+  { _id: "127", user: "902", course: "RS108" },
+
+  // Admin 1000 (sanan) - all courses
+  { _id: "130", user: "1000", course: "RS101" },
+  { _id: "131", user: "1000", course: "RS102" },
+  { _id: "132", user: "1000", course: "RS103" },
+  { _id: "133", user: "1000", course: "RS104" },
+  { _id: "134", user: "1000", course: "RS105" },
+  { _id: "135", user: "1000", course: "RS106" },
+  { _id: "136", user: "1000", course: "RS107" },
+  { _id: "137", user: "1000", course: "RS108" },
 ];
